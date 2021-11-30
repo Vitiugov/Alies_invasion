effb58493cf6d491d1a7dab7475e46a356ff72a1
Vityigov <abyssavior@gmail.com>
2021-11-30 17:06:48 +0300

    13 глава реализация пришельцев(движение построение и уничтожение)

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Group
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 import game_functions as gf
 
@@ -12,17 +13,25 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    bg_color = (230, 230, 230)
+    stats = GameStats(ai_settings)
+    
+
 
     ship = Ship(ai_settings, screen)
-
     bullets = Group()
+    aliens = Group()
+
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
